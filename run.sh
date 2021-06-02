7ed2e0a2598b56e2540e0c219fc5368acc41632c
Cadu Elmadjian <cadu_elmadjian@yahoo.com.br>
2021-06-02 17:01:51 -0300

    suport for tcn non causal

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker run -it --gpus all -v /home/cadu/Nextcloud/Projects/OEMC:/workspace --device /dev/nvidia0 --device /dev/nvidiactl pytorch/pytorch
-docker run -it --gpus all -v /home/cadu/Nextcloud/Projects/OEMC:/workspace --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidiactl pytorch/pytorch
-#docker run -it --gpus all -v /home/cadu/GIT/OEMC:/workspace pytorch/pytorch
+#docker run -it --gpus all -v /home/cadu/Nextcloud/Projects/OEMC:/workspace --device /dev/nvidia0 --device /dev/nvidia-uvm --device /dev/nvidiactl pytorch/pytorch
+docker run -it --gpus all -v /home/cadu/GIT/OEMC:/workspace pytorch/pytorch
